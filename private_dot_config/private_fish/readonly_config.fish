7159249e1714e031c3ed7eafe4360ac744190859
Michael Cramer <mjcramer@gmail.com>
2025-10-03 16:11:50 -0700

    [chezmoi] fix greeting

--- private_dot_config/private_fish/readonly_config.fish
+++ private_dot_config/private_fish/readonly_config.fish
@@ -8,6 +8,11 @@ set -g __fish_config_sourced 1
 if status is-interactive
     # Greeting
     set -g fish_greeting "Welcome to fish, the friendly interactive shell!"
+    set -g fish_greeting (printf '%s%s@%s%s %s%s%s\n%sUptime:%s %s' \
+    	(set_color brblue) (whoami) (hostname -s) (set_color normal) \
+    	(set_color brgreen) (date '+%A, %B %d %Y - %H:%M') (set_color normal) \
+    	(set_color bryellow) (set_color normal) (uptime | sed 's/.*up //' | sed 's/,.*//')
+    )
 
     # ========================================================================
     # Prompt Configuration (Tide)
