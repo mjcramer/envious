67ff7cef5e3ff81bab67e13f971064b3329a0790
Michael Cramer <mjcramer@gmail.com>
2025-10-05 00:47:00 -0700

    disable fisher test

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -16,29 +16,29 @@ if status is-interactive
     	(set_color bryellow) (set_color normal) (uptime | sed 's/.*up //' | sed 's/,.*//')
     )
 
-    # Ensure fisher is installed for plugins (tide, done)
-    if not type -q fisher
-        set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-        curl -sL https://git.io/fisher | source
-        fisher install jorgebucaran/fisher
-    end
+    # # Ensure fisher is installed for plugins (tide, done)
+    # if not type -q fisher
+    #     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    #     curl -sL https://git.io/fisher | source
+    #     fisher install jorgebucaran/fisher
+    # end
 
-    # Install tide and done if missing
-    if not functions -q tide
-        fisher install IlanCosman/tide@v6
-    end
-    if not functions -q __done_initialized
-        fisher install franciscolourenco/done
-    end
+    # # Install tide and done if missing
+    # if not functions -q tide
+    #     fisher install IlanCosman/tide@v6
+    # end
+    # if not functions -q __done_initialized
+    #     fisher install franciscolourenco/done
+    # end
 
-    # Prompt Configuration (Tide)
-    set -g tide_left_prompt_items time pwd git
-    set -g tide_right_prompt_items status cmd_duration jobs python kubectl toolbox terraform aws
-    tide configure --auto --style=Rainbow --show_time='24-hour format' --rainbow_prompt_separators=Round \
-                   --powerline_prompt_heads=Round --powerline_prompt_tails=Sharp \
-                   --powerline_prompt_style='Two lines, frame' --powerline_right_prompt_frame=Yes \
-                   --prompt_connection=Disconnected --prompt_connection_andor_frame_color=Darkest \
-                   --prompt_colors='True color' --prompt_spacing=Compact --icons='Many icons' --transient=Yes
+    # # Prompt Configuration (Tide)
+    # set -g tide_left_prompt_items time pwd git
+    # set -g tide_right_prompt_items status cmd_duration jobs python kubectl toolbox terraform aws
+    # tide configure --auto --style=Rainbow --show_time='24-hour format' --rainbow_prompt_separators=Round \
+    #                --powerline_prompt_heads=Round --powerline_prompt_tails=Sharp \
+    #                --powerline_prompt_style='Two lines, frame' --powerline_right_prompt_frame=Yes \
+    #                --prompt_connection=Disconnected --prompt_connection_andor_frame_color=Darkest \
+    #                --prompt_colors='True color' --prompt_spacing=Compact --icons='Many icons' --transient=Yes
 
     # Aliases
     alias less='less -r'
