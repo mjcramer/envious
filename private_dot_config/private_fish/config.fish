c052021f031c20da15a8f0d41fe2c47ff94b70d2
Michael Cramer <mjcramer@gmail.com>
2025-10-05 00:48:55 -0700

    disable fisher test

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -31,14 +31,14 @@ if status is-interactive
     #     fisher install franciscolourenco/done
     # end
 
-    # # Prompt Configuration (Tide)
-    # set -g tide_left_prompt_items time pwd git
-    # set -g tide_right_prompt_items status cmd_duration jobs python kubectl toolbox terraform aws
-    # tide configure --auto --style=Rainbow --show_time='24-hour format' --rainbow_prompt_separators=Round \
-    #                --powerline_prompt_heads=Round --powerline_prompt_tails=Sharp \
-    #                --powerline_prompt_style='Two lines, frame' --powerline_right_prompt_frame=Yes \
-    #                --prompt_connection=Disconnected --prompt_connection_andor_frame_color=Darkest \
-    #                --prompt_colors='True color' --prompt_spacing=Compact --icons='Many icons' --transient=Yes
+    # Prompt Configuration (Tide)
+    set -g tide_left_prompt_items time pwd git
+    set -g tide_right_prompt_items status cmd_duration jobs python kubectl toolbox terraform aws
+    tide configure --auto --style=Rainbow --show_time='24-hour format' --rainbow_prompt_separators=Round \
+                   --powerline_prompt_heads=Round --powerline_prompt_tails=Sharp \
+                   --powerline_prompt_style='Two lines, frame' --powerline_right_prompt_frame=Yes \
+                   --prompt_connection=Disconnected --prompt_connection_andor_frame_color=Darkest \
+                   --prompt_colors='True color' --prompt_spacing=Compact --icons='Many icons' --transient=Yes
 
     # Aliases
     alias less='less -r'
