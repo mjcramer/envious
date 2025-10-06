98f4939fbb377c76ea097a991364c682774fa0d3
Michael Cramer <mjcramer@gmail.com>
2025-10-06 16:47:06 -0700

    adding install scripts

---
+++ run_onchange_install-brew.darwin.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if ! command -v brew >/dev/null 2>&1; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+brewfile="$(git rev-parse --show-toplevel)/Brewfile"
+brew update
+brew bundle install --file $brewfile && brew bundle cleanup --force --file $brewfile
+
+echo "✅ Brew has finished sucessfully!"
+
+# sudo nano /etc/pam.d/sudo
+# Add this line at the very top:
+# auth       sufficient     pam_tid.so
